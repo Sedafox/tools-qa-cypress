295333a2aec5f4019103d61f5fd782f0d19202b8
sedavirt <usagimadokii@gmail.com>
2022-09-24 17:13:29 -0500

    added more home page features

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -9,4 +9,20 @@ describe('Tests the Home Page', () => {
     it('Should have a Tools QA banner', () => {
         homePage.getBanner().should('be.visible')
     })
+    it('should have a Selenium Online Training banner', () => {
+        homePage.getSeleniumBanner().should('be.visible')
+    })
+    it('should have five card buttons to choose from', () => {
+        homePage.getAllCards().eq(5).should('be.visible')
+        homePage.getAllCards().eq(6).should('not.exist')
+    })
+    it('first card should be Elements', () => {
+        homePage.getFirstCard().should('contain.text','Elements')
+    })
+    it('second card should be Forms' ,() => {
+        homePage.getSecondCard().should('contain.text','Forms')
+    })
+    it('third card should be Alerts, Frame & Windows', () => {
+        homePage.getThirdCard().should('contain.text', 'Alerts, Frame & Windows')
+    })
 })
--- cypress/support/PageObject/homePage.js
+++ cypress/support/PageObject/homePage.js
@@ -4,5 +4,20 @@ class HomePage extends Base {
     getBanner(){
         return cy.get('img[src="/images/Toolsqa.jpg"]')
     }
+    getSeleniumBanner(){
+        return cy.get('img[src="/images/WB.svg"]')
+    }
+    getAllCards(){
+        return cy.get('div[class="card mt-4 top-card"')
+    }
+    getFirstCard(){
+        return this.getAllCards().eq(0)
+    }
+    getSecondCard(){
+        return this.getAllCards().eq(1)
+    }
+    getThirdCard(){
+        return this.getAllCards().eq(2)
+    }
 }
 export default HomePage
